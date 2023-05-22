61fb06966183a0997def93958e1c25d0e51af655
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-05-22 16:01:38 +0700

    17

--- mainIA/Ibu.h
+++ mainIA/Ibu.h
@@ -1,17 +1,21 @@
 #pragma once
 #ifndef IBU_H
 #define IBU_H
+#include <iostream>;
+#include "Anak.h"
+#include <vector>;
+using namespace std;
 
 class ibu {
 public:
-	string nama :
+	string nama;
 	vector<anak*> daftar_anak;
 
 	ibu(string pNama) :nama(pNama) {
 		cout << "Ibu \"" << nama << "\" ada \n";
 	}
 	~ibu() {
-		cut << "Ibu \"" << nama << "\" tidak ada \n";
+		cout << "Ibu \"" << nama << "\" tidak ada \n";
 	}
 	void tambahAnak(anak*);
 	void cetakAnak();
@@ -19,5 +23,13 @@ public:
 
 void ibu::tambahAnak(anak* pAnak) {
 	daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+	cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
 }
 #endif
